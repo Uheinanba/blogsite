c6d0c8910d5c88d1cd8a11526c91c0a525a055db
yuankun <yuankun.yk@alibaba-inc.com>
2018-04-26 17:42:17 +0800

    add demo

---
+++ .vuepress/config.js
@@ -0,0 +1,28 @@
+module.exports = {
+    base: '/blogsite/',
+    title: 'Scotch VuePress',
+    description: "A demo documentation using VuePress",
+    themeConfig:{
+        nav: [
+            { text: 'COUNTER', link: '/counter/' },
+            { text: 'GUIDE', link: '/guide/' },
+        ],
+        sidebar: [
+            {
+              title: 'Counter',
+              collapsable: false,
+              children: [
+                '/counter/counter-app'
+              ]
+            },
+            {
+              title: 'API Guide',
+              collapsable: false,
+              children: [
+                  '/guide/guide',
+                  '/guide/api'
+              ]
+            }
+          ]
+    }
+}
